a5312ddcbf1c0abe8359e91f93acff64943b8c82
JosebaHL09 <71440171+JosebaHL09@users.noreply.github.com>
2021-09-21 08:57:17 +0200

    DB
    datubase 2.0

--- Datubasea/db.sql
+++ Datubasea/db.sql
@@ -30,6 +30,7 @@ CREATE TABLE `erabiltzailea` (
   `Izena` varchar(45) DEFAULT NULL,
   `Abizena` varchar(45) DEFAULT NULL,
   `Posta` varchar(45) DEFAULT NULL,
+  `Pasahitza` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -40,7 +41,7 @@ CREATE TABLE `erabiltzailea` (
 
 LOCK TABLES `erabiltzailea` WRITE;
 /*!40000 ALTER TABLE `erabiltzailea` DISABLE KEYS */;
-INSERT INTO `erabiltzailea` VALUES (1,'Paco12232','Paco','Fetido','dfagdfsgsfa'),(2,'Golondrina777','Juanito','Juan','golondrina@gmail.com');
+INSERT INTO `erabiltzailea` VALUES (1,'Paco12232','Paco','Fetido','dfagdfsgsfa',NULL),(2,'Golondrina777','Juanito','Juan','golondrina@gmail.com',NULL);
 /*!40000 ALTER TABLE `erabiltzailea` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,7 +56,7 @@ CREATE TABLE `erosketa` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_saskia` int NOT NULL,
   `id_produktua` int NOT NULL,
-  `kantitatea` int DEFAULT NULL,
+  `kantitatea` int DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `id_saskia_idx` (`id_saskia`),
   KEY `id_produktua_idx` (`id_produktua`),
@@ -75,6 +76,36 @@ INSERT INTO `erosketa` VALUES (1,1,1,3),(2,1,3,2),(3,1,2,1),(4,2,1,1);
 UNLOCK TABLES;
 
 --
+-- Table structure for table `iruzkinak`
+--
+
+DROP TABLE IF EXISTS `iruzkinak`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `iruzkinak` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `testua` varchar(255) DEFAULT NULL,
+  `id_erabil` int DEFAULT NULL,
+  `id_prod` int DEFAULT NULL,
+  `kalifikazioa` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_erabil_idx` (`id_erabil`),
+  KEY `id_prod_idx` (`id_prod`),
+  CONSTRAINT `id_erabil` FOREIGN KEY (`id_erabil`) REFERENCES `erabiltzailea` (`id`),
+  CONSTRAINT `id_prod` FOREIGN KEY (`id_prod`) REFERENCES `produktua` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `iruzkinak`
+--
+
+LOCK TABLES `iruzkinak` WRITE;
+/*!40000 ALTER TABLE `iruzkinak` DISABLE KEYS */;
+/*!40000 ALTER TABLE `iruzkinak` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `jatetxea`
 --
 
@@ -88,6 +119,7 @@ CREATE TABLE `jatetxea` (
   `Telefonoa` int DEFAULT NULL,
   `Izarrak` int DEFAULT NULL,
   `Oharrak` varchar(255) DEFAULT NULL,
+  `img_path` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -98,7 +130,7 @@ CREATE TABLE `jatetxea` (
 
 LOCK TABLES `jatetxea` WRITE;
 /*!40000 ALTER TABLE `jatetxea` DISABLE KEYS */;
-INSERT INTO `jatetxea` VALUES (1,'Tolibar','Durango',2564814,5,'El mejor bar de durango'),(2,'Bar2','Iurreta',3456575,3,'algo');
+INSERT INTO `jatetxea` VALUES (1,'Tolibar','Durango',2564814,5,'El mejor bar de durango',NULL),(2,'Bar2','Iurreta',3456575,3,'algo',NULL);
 /*!40000 ALTER TABLE `jatetxea` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -114,6 +146,7 @@ CREATE TABLE `produktua` (
   `Izena` varchar(45) DEFAULT NULL,
   `Prezioa` float DEFAULT NULL,
   `id_restaurante` int NOT NULL,
+  `img_path` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_jatetxea_idx` (`id_restaurante`),
   CONSTRAINT `id_jatetxea` FOREIGN KEY (`id_restaurante`) REFERENCES `jatetxea` (`id`)
@@ -126,7 +159,7 @@ CREATE TABLE `produktua` (
 
 LOCK TABLES `produktua` WRITE;
 /*!40000 ALTER TABLE `produktua` DISABLE KEYS */;
-INSERT INTO `produktua` VALUES (1,'Bocadillo',5,1),(2,'Sopa do macaco',10,2),(3,'Nalopitanas',2,1);
+INSERT INTO `produktua` VALUES (1,'Bocadillo',5,1,NULL),(2,'Sopa do macaco',10,2,NULL),(3,'Nalopitanas',2,1,NULL);
 /*!40000 ALTER TABLE `produktua` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -168,4 +201,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-09-20 14:12:56
+-- Dump completed on 2021-09-21  8:55:20
