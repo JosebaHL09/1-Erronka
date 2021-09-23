e95f59cd72d033e325d57673ca31afa704e19b89
JosebaHL09 <71440171+JosebaHL09@users.noreply.github.com>
2021-09-23 14:10:28 +0200

    Limpieza
    lo que pone

--- PythonCRUD/main.py
+++ PythonCRUD/main.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
+#! python
+# -*- coding: UTF-8 -*-# enable debugging
+import cgitb
+import mysql.connector
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="admin123",
+    database="jatetxea"
+)
+def produktuaTabla():
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM produktua")
+    myresult = mycursor.fetchall()
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    s1 = ""
+    for x in myresult:
+        s1 += "<tr>"
+        col = 0
+        for y in x:
+
+            if (col == 4):
+                s1 += "<td><img src='" + str(y) + "' style='width:100px;height:100px;'></td>"
+            else:
+                # print('Y: '+ str(y))
+                s1 += "<td>" + str(y) + "</td>"
+            col += 1
+        s1 += "</tr>"
+    return s1
+def jatetxeaTabla():
+    mycursor2 = mydb.cursor()
+    mycursor2.execute("SELECT * FROM jatetxea")
+    myresult2 = mycursor2.fetchall()
+    s2 = ""
+    for x1 in myresult2:
+        s2 += "<tr>"
+        for y1 in x1:
+            # print('Y: '+ str(y))
+            s2 += "<td>" + str(y1) + "</td>"
+        s2 += "</tr>"
+    return s2
+s2 =jatetxeaTabla()
+s1 =produktuaTabla()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+cgitb.enable()
+print("Content-Type: text/html;charset=utf-8 \n")
+print("<style> table, th, td { border:1px solid black; } </style>")
+print("<TITLE>CGI script output</TITLE>")
+print("<H1>Produktuak</H1>")
+print("<table style='width: 50%'>"+str(s1)+"</table></body></html>")
+print("<H1>Jatetxeak</H1>")
+print("<table style='width: 50%'>"+str(s2)+"</table></body></html>")
