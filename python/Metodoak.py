5258e81f55cbf74c795f5817f3ef544011b4deae
Iribar Gallastegi <iribar.jokin@uni.eus>
2021-09-23 14:06:55 +0200

    ptyhon_xampp
    python eta xamppp konexioa cgitb erabiliz

---
+++ python/Metodoak.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+def sortuErabiltzailea(erabiltzailea, pasahitza, izena, abizena, posta):
+    sql = "INSERT INTO erabiltzailea (erabiltzailea, izena, abizena, posta, pasahitza) VALUES (%s, %s, %s, %s, %s)"
+    val = (erabiltzailea,izena, abizena,posta,pasahitza)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+def sortuIruzkina(kalifikazioa, testua, id_erab, id_prod):
+    sql = "INSERT INTO iruzkinak (testua, id_erabil, id_prod, kalifikazioa) VALUES (%s, %s, %s, %s)"
+    val = (testua, id_erab, id_prod, kalifikazioa)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+def sortuSaskia(helbidea, telefonoa, data, id_erabil):
+    sql = "INSERT INTO saskia (helbidea, telefonoa, data, id_erabiltzaile) VALUES (%s, %s, %s, %s)"
+    val = (helbidea, telefonoa, data, id_erabil)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+def sortuErosketa(id_saskia, id_produktua, kantitatea):
+    sql = "INSERT INTO erosketa (id_saskia, id_produktua, kantitatea) VALUES (%s, %s, %s)"
+    val = (id_saskia, id_produktua, kantitatea)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="jatetxea"
+)
+mycursor = mydb.cursor()
+
+#sortuErabiltzailea("Manolo2354","manolo123","Manolo","Lopez","Manolete@gmail.com")
+#sortuIruzkina(9,"Muy buen producto, extremadamente rico y todo bien preparado, recomendable 100%",1,1)
+#sortuSaskia("c/Landako etorbidea 20 4E,Durango",655495415,"2021-09-27",1)
+#sortuErosketa(3,1,4)
