bdf0626d8e84554da8fe240731833a21dc903c86
JosebaHL09 <71440171+JosebaHL09@users.noreply.github.com>
2021-09-27 08:22:14 +0200

    More js
    more

---
+++ JS_Functions/JS_Joseba/scripts.js
@@ -0,0 +1,33 @@
+function balidatu() {
+    var userArray = ["Paco", "Admin"]
+    var passwordArray = ["123", "admin123"]
+
+    var usuario = document.getElementById('usuario_txt').value
+    var contrasena = document.getElementById('contraseña_txt').value
+    var valid = false;
+    for (i = 0; i < userArray.length; i++) {
+        if ((usuario == userArray[i]) && (contrasena == passwordArray[i])) {
+            valid = true
+            break
+        }
+    }
+    if (valid) {
+        alert("Bienvenido!")
+    } else {
+        alert("usuario no encontrado: " + usuario + " - " + contrasena)
+    }
+
+
+}
+
+function gorde() {
+    const user = {
+        username: document.getElementById('erabiltzailea_txt').value,
+        pasahitza: document.getElementById('pasahitza_txt').value,
+        izena: document.getElementById('izena_txt').value,
+        abizena: document.getElementById('abizena_txt').value,
+        posta: document.getElementById('posta_txt').value
+    }
+
+    alert("Egun on " + user.izena + " " + user.abizena) 
+}
