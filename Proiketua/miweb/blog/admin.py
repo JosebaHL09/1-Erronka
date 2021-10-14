bf95fd725f4c4efd6aabdd5cde0d99c9bcb8cda5
JosebaHL09 <71440171+JosebaHL09@users.noreply.github.com>
2021-10-14 13:31:38 +0200

    models

--- Proiketua/miweb/blog/admin.py
+++ Proiketua/miweb/blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Erabiltzailea
 # Register your models here.
+admin.site.register(Erabiltzailea)
--- Proiketua/miweb/blog/models.py
+++ Proiketua/miweb/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+class Erabiltzailea(models.Model):
+    erabiltzailea = models.CharField(max_length=1000)
+    izena = models.CharField(max_length=1000)
+    abizena = models.CharField(max_length=1000)
+    posta = models.EmailField(max_length=1000)
+    pasahitza = models.CharField(max_length=1000)
+
+    def __unicode__(self):
+        return self.erabiltzailea
