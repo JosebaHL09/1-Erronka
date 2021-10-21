9a229d9b5ea832a0310ebf8ff9085281b2c45dd3
Mikel Mañeru <manerugilmikel@gmail.com>
2021-10-21 09:21:21 +0200

    ProiektuaV1

---
+++ JJM_Delivery/jjm/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Erabiltzailea)
+admin.site.register(Saskia)
+admin.site.register(Jatetxea)
+admin.site.register(Produktua)
+admin.site.register(Erosketa)
+admin.site.register(Iruzkina)
+# Register your models here.
---
+++ JJM_Delivery/jjm/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django import forms
+# Create your models here.
+
+class Erabiltzailea(models.Model):
+    erabiltzailea = models.CharField(max_length=1000)
+    izena = models.CharField(max_length=1000)
+    abizena = models.CharField(max_length=1000)
+    posta = models.EmailField(max_length=1000)
+    pasahitza = models.CharField(max_length=1000)
+    def __unicode__(self):
+        return self.erabiltzailea
+
+class Saskia(models.Model):
+    helbidea = models.CharField(max_length=1000)
+    telefonoa = models.IntegerField()
+    data = models.DateTimeField()
+    erabiltzailea = models.ForeignKey(Erabiltzailea,related_name='erabiltzailea_saskia',on_delete =models.CASCADE)
+
+    def __unicode__(self):
+        return self.helbidea
+
+class Jatetxea(models.Model):
+    izena = models.CharField(max_length=1000)
+    deskripzioa=models.CharField(max_length=1000)
+    helbidea = models.CharField(max_length=1000)
+    telefonoa = models.IntegerField()
+    mota = models.CharField(max_length=1000)
+    oharrak = models.CharField(max_length=1000)
+    img_path = models.CharField(max_length=1000)
+    distantzia=models.IntegerField()
+
+
+    def __unicode__(self):
+        return self.izena
+
+class Produktua(models.Model):
+    izena = models.CharField(max_length=1000)
+    prezioa = models.FloatField()
+    mota = models.CharField(max_length=1000)
+    deskontua = models.BooleanField()
+    portzentaia = models.FloatField(blank=True, null=True)
+    jatetxea = models.ForeignKey(Jatetxea, related_name='jatetxea_produktua', on_delete=models.CASCADE)
+    img_path = models.CharField(max_length=1000)
+
+    def __unicode__(self):
+        return self.izena
+
+class Erosketa(models.Model):
+    saskia = models.ForeignKey(Saskia, related_name='saskia', on_delete=models.CASCADE)
+    produktua = models.ForeignKey(Produktua, related_name='produktua_erosketa', on_delete=models.CASCADE)
+    kantitatea = models.IntegerField()
+
+    def __unicode__(self):
+        return self.izena
+
+class Iruzkina(models.Model):
+    testua = models.CharField(max_length=1000)
+    erabiltzailea = models.ForeignKey(Erabiltzailea, related_name='erabiltzailea_iruzkina', on_delete=models.CASCADE)
+    jatetxea = models.ForeignKey(Jatetxea, related_name='jatetxea_iruzkina', on_delete=models.CASCADE)
+    kalifikazioa = models.IntegerField()
+
+    def __unicode__(self):
+        return self.testua + self.erabiltzailea
