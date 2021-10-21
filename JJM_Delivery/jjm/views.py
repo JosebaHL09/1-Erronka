f81854a282df4bbc04a04712b7e85502fb7172a6
JosebaHL09 <71440171+JosebaHL09@users.noreply.github.com>
2021-10-21 14:28:46 +0200

    contraseña y algunas cositas

--- JJM_Delivery/jjm/views.py
+++ JJM_Delivery/jjm/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from django.http import *
+from django.shortcuts import render, redirect
+
+from .forms import *
+
 
 # Create your views here.
+def erabiltzailea_new(request):
+    if request.method == 'POST':
+        form = ErabiltzaileaForm(request.POST)
+        if form.is_valid():
+            erabiltzailea = form.save(commit=False)
+            erabiltzailea.pasahitza = make_password("123")
+            erabiltzailea.save()
+            return HttpResponseRedirect('../..')
+    else:
+        form = ErabiltzaileaForm()
+        redirect('index.html')
+    return render(request, 'erabiltzailea_edit.html', {'form':form})
