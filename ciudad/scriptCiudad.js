f97f117d5c606b80a1e9c8932c8d7911979c6ddf
Mikel Mañeru <manerugilmikel@gmail.com>
2021-10-27 08:45:07 +0200

    PRE-ALPHA

---
+++ ciudad/scriptCiudad.js
@@ -0,0 +1,76 @@
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (window.scrollY >= 20 ) {
+        document.getElementById("navbar").style.background = "#FFE48F";
+        document.getElementById("nav__link").style.color = "#111111";
+        document.getElementById("nav__link1").style.color = "#111111";
+        document.getElementById("nav__link2").style.color = "#111111";
+        document.getElementById("titulo").style.background = "red";
+    }else{
+        document.getElementById("navbar").style.background = "transparent";
+        document.getElementById("nav__link").style.color = "#C4C6C7";
+        document.getElementById("nav__link1").style.color = "#C4C6C7";
+        document.getElementById("nav__link2").style.color = "#C4C6C7";
+        document.getElementById("titulo").style.background = "trasparent";
+ 
+    }
+}
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
+
+navToggle.addEventListener("click", () => {
+    document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove("nav-open");
+    });
+});
+//Mapas
+var mymap = L.map('mapid').setView([43.176749, -2.496166], 15);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlpa2VsbTEiLCJhIjoiY2t2MHNwOTN5MWF3eTJ2cXE4NmJueDVjbiJ9.MUhKjmQk_k9sx8s54IMZug', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+function calcCrow(lat1, lon1, lat2, lon2) 
+{
+  var R = 6371; // km
+  var dLat = toRad(lat2-lat1);
+  var dLon = toRad(lon2-lon1);
+  var lat1 = toRad(lat1);
+  var lat2 = toRad(lat2);
+
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c;
+  return d;
+}
+
+function toRad(Value) 
+{
+    return Value * Math.PI / 180;
+}
+
+
+function closePopUp(){
+    $("#pop-up").fadeOut();
+}
+function showSearch(){
+    document.getElementById('searchId').style.display="block";
+
+}
+
+  
